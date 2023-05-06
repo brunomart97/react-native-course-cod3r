398807a91974209baa7b6216a792b0a9ca46fceb
Bruno Martins <brunomart97@hotmail.com>
2023-05-06 16:49:14 -0300

    refactor: standardized code indentation

--- calculator/.prettierrc.js
+++ calculator/.prettierrc.js
@@ -2,6 +2,8 @@ module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
   bracketSpacing: false,
-  singleQuote: true,
   trailingComma: 'all',
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
 };
